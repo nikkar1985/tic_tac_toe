f87dadd474d84dc6a117c8a514c057f448f87cf0
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-06-02 19:13:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');s
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
